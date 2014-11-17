3a73be5570803a3e7cb994cb76da9403d6fd3715
Raymundo LapDell <raymundoc.vela@gmail.com>
2014-11-17 02:10:17 -0600

    conectar.php
    Estaba excluido para la sincronizacion con github, vimos propiedades del
    proyecto y se eliminó la restrición

---
+++ conectar.php
@@ -0,0 +1,9 @@
+<?php
+$namedb="terrasat_mobilehunt";
+$server="terrasat.com.mx";
+$usr="terrasat_ray";
+$psw="ProAn2014" ;
+$con=mysql_connect($server,$usr,$psw) or die;
+$namedb="terrasat_mobilehunt";
+mysql_selectdb($namedb);
+?>
